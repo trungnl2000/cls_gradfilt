299cd578327472f1f8c53e8834569a7290433087
trungnl2000 <trungnl2000@gmail.com>
2024-08-09 11:28:14 +0200

    .

--- scripts/SVD_with_var_compression/mcunet/c10_mcunet_var0.8.sh
+++ scripts/SVD_with_var_compression/mcunet/c10_mcunet_var0.8.sh
@@ -10,7 +10,7 @@ usr_group_kl="full_pretrain_imagenet"
 # load_args="--model.load pretrained_ckpts/mcu/pretrain_15.29_cifar10/version_0/checkpoints/epoch=49-val-acc=0.950.ckpt"
 
 general_config_args="--config configs/mcunet_config.yaml"
-logger_args="--logger.save_dir delte/mcunet/$dataset/SVD/var0.8"
+logger_args="--logger.save_dir runs/mcunet/$dataset/SVD/var0.8"
 data_args="--data.name $dataset --data.data_dir data/$dataset --data.train_workers 24 --data.val_workers 24" # --data.partition 1 --data.usr_group data/$dataset/usr_group_${usr_group_kl}.npy"
 trainer_args="--trainer.max_epochs 50"
 model_args="--model.SVD_var 0.8 --model.with_SVD_with_var_compression True --model.set_bn_eval True --model.use_sgd True --model.learning_rate 0.05 --model.num_classes $num_classes --model.momentum 0 --model.anneling_steps 50 --model.scheduler_interval epoch --trainer.gradient_clip_val 2.0"
@@ -21,7 +21,7 @@ common_args="$general_config_args $trainer_args $data_args $model_args $load_arg
 echo $common_args
 
 # python trainer_cls.py ${common_args} --logger.exp_name SVD_l1_var0.8_${usr_group_kl} --model.num_of_finetune 1 
-python trainer_cls.py ${common_args} --logger.exp_name SVD_l2_var0.8_${usr_group_kl} --model.num_of_finetune 2
+# python trainer_cls.py ${common_args} --logger.exp_name SVD_l2_var0.8_${usr_group_kl} --model.num_of_finetune 2
 # python trainer_cls.py ${common_args} --logger.exp_name SVD_l3_var0.8_${usr_group_kl} --model.num_of_finetune 3
 # python trainer_cls.py ${common_args} --logger.exp_name SVD_l4_var0.8_${usr_group_kl} --model.num_of_finetune 4
 # python trainer_cls.py ${common_args} --logger.exp_name SVD_l5_var0.8_${usr_group_kl} --model.num_of_finetune 5
